962ad1493bd78734d82f3d2d2ccd0bee38fdbdd4
rodralez <rodralez@gmail.com>
2019-10-25 18:09:48 -0300

    2019-10-25

--- 29-DSP_Floating-point/ex_01.c
+++ 29-DSP_Floating-point/ex_01.c
@@ -3,11 +3,15 @@
 #include <math.h>
 #include <fenv.h>
 
+// compile: gcc -Wall -O3 -std=c99 ex_01.c -o ex_01
+ 
+float mysum(float x, unsigned int N);
+
 int main(void)
 {	
 	float a, b, c, f1, f2;
 	double d1;
-	unsigned long int i;
+	unsigned long int N;
 	
 	a = 1000000000.0;
 	b =   20000000.0;
@@ -24,22 +28,36 @@ int main(void)
 	
 	printf("Error en f1 = %10e \n", f1 - 400000000000000000000000.0 );
 	printf("Error en f2 = %10e \n", f2 - 400000000000000000000000.0 );
-	printf("Error en d1 = %20e \n", d1 - 400000000000000000000000.0 );
+	printf("Error en d1 = %10e \n", d1 - 400000000000000000000000.0 );
 	
-	double acum_1, acum_2;
+	float acum_1, acum_2;
+	
+	N = 10000;
 	
 	acum_1 = 0.0;
-	for (i = 0; i < 10000000; i++){ acum_1 += 0.01; } 
+	acum_1 = mysum( 0.01, N );
+	printf("\nacum_1 = %.20f \n", acum_1 );
+	printf("Error en acum_1 = %.20f \n", acum_1 - ( 100 ));
 
 	acum_2 = 0.0;
-	b = 0.333;
-	for (i = 0; i < 10000000; i++){ acum_2 += b / b; }
-	
-	printf("acum_1 = %f \n", acum_1 );
-	printf("acum_2 = %f \n", acum_2 );
-	
-	printf("Error en acum_1 = %.20lf \n", acum_1 - (100000.0 ));
-	printf("Error en acum_2 = %.20lf \n", acum_2 - (10000000.0));
+	acum_2 = fabsf ( (N/10) - mysum(0.1, N) ) ;
+	printf("Error en acum_2 = %.20f \n", acum_2 - (0.0));
 	
 	return 0;
 }
+
+float mysum(float x, unsigned int N)
+{
+		float acum;
+		unsigned int i;
+		
+		acum = 0.0;
+		
+		for (i = 0; i < N; i++)
+		{ 
+			acum += x;
+		}
+	
+	return acum;
+}
+
--- 29-DSP_Floating-point/ex_04.c
+++ 29-DSP_Floating-point/ex_04.c
@@ -10,41 +10,24 @@
 #define _ISOC99_SOURCE
 #include <fenv.h>
 
-void show_fe_exceptions(void)
-{
-	int raised;
-	
-    printf("Current exceptions raised: ");
-    
-    raised = fetestexcept (FE_DIVBYZERO);
-    if(raised & FE_DIVBYZERO)     printf("FE_DIVBYZERO");
-    
-    raised = fetestexcept (FE_INEXACT);
-    if(raised & FE_INEXACT)       printf("FE_INEXACT");
-    
-    raised = fetestexcept (FE_INVALID);
-    if(raised & FE_INVALID)       printf("FE_INVALID");
-    
-    raised = fetestexcept (FE_OVERFLOW);
-    if(raised & FE_OVERFLOW)      printf("FE_OVERFLOW");
-    
-    raised = fetestexcept (FE_UNDERFLOW);
-    if(raised & FE_UNDERFLOW)     printf("FE_UNDERFLOW");
-    
-    raised = fetestexcept (FE_ALL_EXCEPT);
-    if( (raised & FE_ALL_EXCEPT) == 0) printf("None exceptions");
-    
-    printf("\n");
-}
-     
+/*  FUNCTION DECLARED IN fenv.h */
+
+// int feclearexcept(int excepts);
+// int fegetexceptflag(fexcept_t *flagp, int excepts);
+// int feraiseexcept(int excepts);
+// int fesetexceptflag(const fexcept_t *flagp, int excepts);
+// int fetestexcept(int excepts);
+// int fegetround(void);
+// int fesetround(int rounding_mode);
+// int fegetenv(fenv_t *envp);
+// int feholdexcept(fenv_t *envp);
+// int fesetenv(const fenv_t *envp);
+// int feupdateenv(const fenv_t *envp);
+       
+void show_fe_exceptions(void);
+        
 int main(void)
 {	
-	int ROUND_MODE;
-	float a, b;
-  
-	ROUND_MODE = fegetround();		
-	printf("Current Round Mode = %d \n", ROUND_MODE );
-		
     show_fe_exceptions();
       
     /* Temporarily raise other exceptions */
@@ -70,3 +53,37 @@ int main(void)
 
 	return 0;	
 }
+
+void show_fe_exceptions(void)
+{
+	int raised;
+	
+  // FE_DIVBYZERO	Pole error: division by zero, or some other asymptotically infinite result (from finite arguments).
+  // FE_INEXACT	Inexact: the result is not exact.
+  // FE_INVALID	Domain error: At least one of the arguments is a value for which the function is not defined.
+  // FE_OVERFLOW	Overflow range error: The result is too large in magnitude to be represented as a value of the return type.
+  // FE_UNDERFLOW	Underflow range error: The result is too small in magnitude to be represented as a value of the return type.
+
+    printf("Current exceptions raised: ");
+    
+    raised = fetestexcept (FE_DIVBYZERO);
+    if(raised & FE_DIVBYZERO)     printf("FE_DIVBYZERO");
+    
+    raised = fetestexcept (FE_INEXACT);
+    if(raised & FE_INEXACT)       printf("FE_INEXACT");
+    
+    raised = fetestexcept (FE_INVALID);
+    if(raised & FE_INVALID)       printf("FE_INVALID");
+    
+    raised = fetestexcept (FE_OVERFLOW);
+    if(raised & FE_OVERFLOW)      printf("FE_OVERFLOW");
+    
+    raised = fetestexcept (FE_UNDERFLOW);
+    if(raised & FE_UNDERFLOW)     printf("FE_UNDERFLOW");
+    
+    raised = fetestexcept (FE_ALL_EXCEPT);
+    if( (raised & FE_ALL_EXCEPT) == 0) printf("None exceptions");
+    
+    printf("\n");
+}
+
