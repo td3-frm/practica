25f3f7cc458cac27fdc799cfb453db635884972f
rodralez <rodralez@gmail.com>
2019-11-07 21:21:08 -0300

    2019-11-07

--- 31-DSP_Filtros-FIR/ej_01.m
+++ 31-DSP_Filtros-FIR/ej_01.m
@@ -19,7 +19,7 @@ signal_n = mynoise(signal, snr);
 
 %% MA
 
-N = 10;
+N = 50;
 a = 1;
 b = ones(1, N) / N;
 
@@ -42,4 +42,16 @@ legend('ORG', 'NOISE', 'MA')
 
 figure 
 freqz(b, a, 256)
+
+%% 
+
+[f, dft_mag, dft_phase, dft, NFFT] = my_dft(signal_n, fs);
+[f_ma, dft_mag_ma, dft_phase, dft, NFFT] = my_dft(signal_ma, fs);
+
+figure
+plot(f, dft_mag, '-ob')
+hold on
+plot(f_ma, dft_mag_ma,'--g', 'linewidth', 2)
+
+
 
