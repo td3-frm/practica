71d1b5f0c10c3ca3e7938e1830fd3dd54b766667
Rodrigo <rodralez@gmail.com>
2021-11-02 19:09:09 -0300

    2021-11-02

--- 32-DSP_Filtros-IIR/c-files/iir_filter.h
+++ 32-DSP_Filtros-IIR/c-files/iir_filter.h
@@ -1,11 +1,11 @@
 /* ========================================================================
- * iir_filters.h
+ * iir_filter.h
  *
  * Functions inspired in IIR_filters_fourth_order.c (v 1.00) by Richard Sikora
  * from Texas Instruments.
  * 
  * Version: 001
- * Date:    2016/11/10
+ * Date:    2021/11/02
  * Author:  Rodrigo Gonzalez <rodralez@frm.utn.edu.ar>
  * URL:     https://github.com/rodralez/td3
  *
@@ -13,6 +13,12 @@
  
 #ifndef IIR_FILTERS_H
 #define IIR_FILTERS_H
+
+typedef struct irr_stru irr_stru;
+
+irr_stru * iir_get_sos(void);
+void iir_2do_df1_float(float *input, float *output);
+void iir_2do_df2_float(float *input, float *output);
 
 void iir_filter_I_float (const float * input, const float * coeff,  const float * gain, const unsigned int N, float * output);
 void iir_filter_II_float(const float * input, const float * coeff,  const float * gain, const unsigned int N, float * output);
