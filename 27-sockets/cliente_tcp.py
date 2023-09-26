3aff47da5c78a50f9a82e9c1377da113555916ec
Rodrigo Gonzalez <rodralez@gmail.com>
2023-09-26 17:19:21 -0300

    2023-09-26

---
+++ 27-sockets/cliente_tcp.py
@@ -0,0 +1,18 @@
+import socket
+
+# Configuración del cliente
+host = '127.0.0.1'  # Dirección IP del servidor (localhost en este caso)
+port = 12345       # Puerto al que se conectará el cliente
+
+# Crear un socket TCP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conectar al servidor
+client_socket.connect((host, port))
+
+# Enviar datos al servidor
+mensaje = "Hola, servidor TCP"
+client_socket.send(mensaje.encode('utf-8'))
+
+# Cerrar la conexión con el servidor
+client_socket.close()
