e08cd3a20b08e7147b28e277614218a4fefe1311
latuquen <latuquen@gmail.com>
2024-04-09 14:26:22 -0300

    2024-04-09

--- 07-cola-mensajes/mq01.c
+++ 07-cola-mensajes/mq01.c
@@ -16,63 +16,63 @@
 
 int main() {
    
-   int err, leido;
-   char buff[1024];   
-   mqd_t mqd; 
-   struct mq_attr attr, attr_rcv;
+	int err, leido;
+	char buff[1024];   
+	mqd_t mqd; 
+	struct mq_attr attr, attr_rcv;
   
-   // Se fijan algunos parametros de la cola de mensajes antes de crearla
-   attr.mq_msgsize = sizeof(buff);
-   attr.mq_maxmsg = 5;
+	// Se fijan algunos parametros de la cola de mensajes antes de crearla
+	attr.mq_msgsize = sizeof(buff);
+	attr.mq_maxmsg = 5;
    
-   // Se crea cola de mensajes
-   mqd = mq_open(MQ_PATH, O_RDWR | O_CREAT, 0777, &attr); 
-   if (mqd < 0) {
-      printf ("error en mq_open()\n");   
-      exit(-1); }
+	// Se crea cola de mensajes
+	mqd = mq_open(MQ_PATH, O_RDWR | O_CREAT, 0777, &attr); 
+	if (mqd < 0) {
+		printf ("error en mq_open()\n");   
+		exit(-1); }
       
-   printf("Cola de mensajes creada\n"); 
+	printf("Cola de mensajes creada\n"); 
    
-   // Escribe en cola de mensajes
-   err = mq_send(mqd, MENSAJE, strlen(MENSAJE)+1, 1);  //strlen nos da la longitud de una cadena
-   if(err == -1){
+	// Escribe en cola de mensajes
+	err = mq_send(mqd, MENSAJE, strlen(MENSAJE)+1, 1);  //strlen nos da la longitud de una cadena
+	if(err == -1){
       printf ("error en mq_send()\n");
       exit(-1);    }
 
-   printf("Mensaje enviado (%d)\n", err);
+	printf("Mensaje enviado (%d)\n", err);
    
-   // Se leen parametros de la cola de mensajes
-   if (mq_getattr(mqd, &attr_rcv) == -1){
-      printf ("error en mq_getattr()\n");
-      exit(-1); }
+	// Se leen parametros de la cola de mensajes
+	if (mq_getattr(mqd, &attr_rcv) == -1){
+		printf ("error en mq_getattr()\n");
+		exit(-1); }
       
-   printf("Nro max. de mensajes en cola de mensajes: %ld\n", attr_rcv.mq_maxmsg);
-   printf("Longitud max. de mensaje: %ld\n", attr_rcv.mq_msgsize);
-   printf("Nros de mensajes pendientes en cola de mensajes: %ld\n", attr_rcv.mq_curmsgs);
+	printf("Nro max. de mensajes en cola de mensajes: %ld\n", attr_rcv.mq_maxmsg);
+	printf("Longitud max. de mensaje: %ld\n", attr_rcv.mq_msgsize);
+	printf("Nros de mensajes pendientes en cola de mensajes: %ld\n", attr_rcv.mq_curmsgs);
 
-   // Se lee de cola de mensajes
-   leido = mq_receive(mqd, buff, attr_rcv.mq_msgsize, 0);
-   if (( leido < 0 )){
-      printf ("error en mq_receive()\n");
-      exit(-1); }
+	// Se lee de cola de mensajes
+	leido = mq_receive(mqd, buff, attr_rcv.mq_msgsize, 0);
+	if (( leido < 0 )){
+		printf ("error en mq_receive()\n");
+		exit(-1); }
 
-   printf("Mensaje leido: %s\n", buff);
+	printf("Mensaje leido: %s\n", buff);
    
-   // Se cierra cola de mensajes
-   err = mq_close(mqd);
-   if (( err < 0 )){
-      printf ("error en mq_close()\n");
-      exit(-1);   }
+	// Se cierra cola de mensajes
+	err = mq_close(mqd);
+	if (( err < 0 )){
+		printf ("error en mq_close()\n");
+		exit(-1);   }
 
-      printf("Cola de mensajes cerrada (%d)\n", err);
+	printf("Cola de mensajes cerrada (%d)\n", err);
    
-   // Se elimina cola de mensajes
-   err = mq_unlink(MQ_PATH);
-   if(err == -1){
-      printf ("error en mq_unlink()\n");
-      exit(-1);   }
+	// Se elimina cola de mensajes
+	err = mq_unlink(MQ_PATH);
+	if(err == -1){
+		printf ("error en mq_unlink()\n");
+		exit(-1);   }
 
-      printf("Cola de mensajes eliminada (%d)\n", err);
+	printf("Cola de mensajes eliminada (%d)\n", err);
       
-   exit(0);
+	exit(0);
 }
