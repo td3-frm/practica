2b5cc652135444fe5d0856d61788697b15d5f816
latuquen <latuquen@gmail.com>
2024-04-09 14:27:20 -0300

    2024-04-09

--- 07-cola-mensajes/mq05.c
+++ 07-cola-mensajes/mq05.c
@@ -25,13 +25,13 @@
 
 void escribe_mensaje(int a){ 
 
-   write(STDOUT_FILENO, "\nenviar mensaje\n", sizeof("\nenviar mensaje\n"));
+	write(STDOUT_FILENO, "\nenviar mensaje\n", sizeof("\nenviar mensaje\n"));
 
-   //-- Escribe en cola de mensajes --------------------------
-   err = mq_send(mqd, MENSAJE, strlen(MENSAJE)+1, 1);  //strlen nos da la longitud de una cadena
-   if(err == -1){
+	//-- Escribe en cola de mensajes --------------------------
+	err = mq_send(mqd, MENSAJE, strlen(MENSAJE)+1, 1);
+	if(err == -1){
         write(STDOUT_FILENO, "\nerror en mq_send()\n", sizeof("\nerror en mq_send()\n"));  }
-   else {
+	else {
         write(STDOUT_FILENO, "\nMensaje enviado\n", sizeof("\nMensaje enviado\n"));   }
 
 }
@@ -39,59 +39,58 @@ void escribe_mensaje(int a){
 
 void finaliza_proceso(int a){ 
 
-   err = mq_close(mqd);
-   if (( err < 0 )){
-      write(STDOUT_FILENO, "\nerror en mq_close()\n", sizeof("\nerror en mq_close()\n"));
-      exit(0);   }
+	err = mq_close(mqd);
+	if (( err < 0 )){
+		write(STDOUT_FILENO, "\nerror en mq_close()\n", sizeof("\nerror en mq_close()\n"));
+		exit(0);   }
 
-   write(STDOUT_FILENO, "\nCola de mensajes cerrada\n", sizeof("\nCola de mensajes cerrada\n"));
+	write(STDOUT_FILENO, "\nCola de mensajes cerrada\n", sizeof("\nCola de mensajes cerrada\n"));
 
-   err = mq_unlink(MQ_PATH);
-   if(err == -1){
-      write(STDOUT_FILENO, "\nerror en mq_unlink()\n", sizeof("\nerror en mq_unlink()\n"));
-      exit(0);  }
+	err = mq_unlink(MQ_PATH);
+	if(err == -1){
+		write(STDOUT_FILENO, "\nerror en mq_unlink()\n", sizeof("\nerror en mq_unlink()\n"));
+		exit(0);  }
 
-   write(STDOUT_FILENO, "\nCola de mensajes eliminada\n", sizeof("\nCola de mensajes eliminada\n"));
+	write(STDOUT_FILENO, "\nCola de mensajes eliminada\n", sizeof("\nCola de mensajes eliminada\n"));
 
-   write(STDOUT_FILENO, "\nTerminando proceso...\n", sizeof("\nTerminando proceso...\n"));
+	write(STDOUT_FILENO, "\nTerminando proceso...\n", sizeof("\nTerminando proceso...\n"));
 
-   exit(0);
+	exit(0);
 }
 
 
 int main() {
    
-   signal(SIGINT, escribe_mensaje);    //Ctrl + C
-   signal(SIGUSR1, finaliza_proceso);  //termina
+	signal(SIGINT, escribe_mensaje);    //Ctrl + C
+	signal(SIGUSR1, finaliza_proceso);  //termina
 
-   printf("Soy el proceso: %d \n ",getpid());
+	printf("Soy el proceso: %d \n ",getpid());
 
-   mq_unlink(MQ_PATH);
+	mq_unlink(MQ_PATH);  //elimino la cola de mensajes por si existe 
 
-   attr.mq_msgsize = sizeof(buff);
-   attr.mq_maxmsg = 5;
+	attr.mq_msgsize = sizeof(buff);
+	attr.mq_maxmsg = 5;
    
-   // abrir y crear cola de mensajes
+	// abrir y crear cola de mensajes
     mqd = mq_open(MQ_PATH, O_RDWR | O_CREAT , 0666, &attr);
-   if (mqd < 0) {
-      printf ("error en mq_open()");   
-      exit(-1);  }
+	if (mqd < 0) {
+		printf ("error en mq_open()");   
+		exit(-1);  }
 
-   printf("Cola de mensajes creada\n");
+	printf("Cola de mensajes creada\n");
 
- 
-  if (mq_getattr(mqd, &attr_rcv) == -1) {
-      printf ("error en mq_getattr()");
-      exit(-1); }
-   printf("Nro max. de mensajes en cola de mensajes: %ld\n", attr_rcv.mq_maxmsg);
-   printf("Longitud max. de mensaje: %ld\n", attr_rcv.mq_msgsize);
-   printf("Nros de mensajes pendientes en cola de mensajes: %ld\n", attr_rcv.mq_curmsgs);
+ 	if (mq_getattr(mqd, &attr_rcv) == -1) {
+		printf ("error en mq_getattr()");
+		exit(-1); }
+   
+	printf("Nro max. de mensajes en cola de mensajes: %ld\n", attr_rcv.mq_maxmsg);
+	printf("Longitud max. de mensaje: %ld\n", attr_rcv.mq_msgsize);
+	printf("Nros de mensajes pendientes en cola de mensajes: %ld\n", attr_rcv.mq_curmsgs);
 
-
-   while(1){
+	while(1){
 
        
-   }  //fin while
+	}  //fin while
 
-   exit(0);
+	exit(0);
 }
