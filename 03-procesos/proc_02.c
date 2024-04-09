7d425fe8bde0085e930252e2e5d325ab11571ba4
latuquen <latuquen@gmail.com>
2024-04-09 15:00:25 -0300

    2024-04-09

--- 03-procesos/proc_02.c
+++ 03-procesos/proc_02.c
@@ -11,24 +11,11 @@ int i;
 
 int main (){
 
-<<<<<<< HEAD
-	pid_t pid;
-	int i;
-
-	pid = fork();
-	
-	if (pid == 0) 
-		printf ("Mi pid es %d y el pid de papa es %d. fork() devolvio %d\n", getpid(), getppid(), pid);
-	else
-		printf ("Mi pid es %d y el pid de papa es %d. fork() devolvio %d\n", getpid(), getppid(), pid);
-	
-=======
 	printf ("Proceso unico: Mi pid es %d\n", getpid());
 	
 	pid = fork();		
 	printf ("Mi pid es %d y el pid de papa es %d. fork() devolvio %d\n", getpid(), getppid(), pid);
 		
->>>>>>> 242b4904c0f15933c5411db1a1ef1eb693788013
 	// Ejecute pstree en otra consola	
 	sleep(30); 
 	
